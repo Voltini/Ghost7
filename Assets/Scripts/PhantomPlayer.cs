e8633a4f006ce83e0b168dc9dbd83814d507a52d
Clavel Xavier <xavier.clavel@etu.inp-n7.fr>
2021-12-08 01:25:42 +0100

    minor changes

--- Assets/Scripts/PhantomPlayer.cs
+++ Assets/Scripts/PhantomPlayer.cs
@@ -7,7 +7,7 @@ public class PhantomPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     // Update is called once per frame
